c3e0168dd9f586a72f379886d2108c71a8490eff
Igor Afanasyev <igor.afanasyev@gmail.com>
2021-06-20 22:55:37 -0700

    Change boot script names to match the updated Docker image

--- serge-shell.cmd
+++ serge-shell.cmd
@@ -1,10 +1,10 @@
 @echo off
 
-set PARAMS="--entrypoint /usr/local/bin/boot/run-in-docker.sh"
+set PARAMS="--entrypoint /usr/local/bin/boot/run-command"
 if "%~1"=="" (
     echo "No arguments provided, will run an interactive shell"
     echo Mounting %~dp0 as /root
-    set PARAMS="-it --entrypoint /usr/local/bin/boot/serge-shell.sh"
+    set PARAMS="-it --entrypoint /usr/local/bin/boot/run-shell"
 )
 
 docker run ^

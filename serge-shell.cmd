b20ce5b3cca93348615537d6f7352f7e7f553378
Igor Afanasyev <igor.afanasyev@gmail.com>
2021-07-28 11:59:26 -0700

    Fix the command line script for Windows

--- serge-shell.cmd
+++ serge-shell.cmd
@@ -1,10 +1,10 @@
 @echo off
 
-set PARAMS="--entrypoint /usr/local/bin/boot/run-command"
+set PARAMS=--entrypoint /usr/local/bin/boot/run-command
 if "%~1"=="" (
     echo "No arguments provided, will run an interactive shell"
     echo Mounting %~dp0 as /root
-    set PARAMS="-it --entrypoint /usr/local/bin/boot/run-shell"
+    set PARAMS=-it --entrypoint /usr/local/bin/boot/run-shell
 )
 
 docker run ^
@@ -12,5 +12,5 @@ docker run ^
     -v "%~dp0:/root" ^
     %PARAMS% ^
     --env PS1="\[\033[01;35m\][Serge Shell]\[\033[00m\] \w \$ " ^
-    smartcatcom/serge:v2
+    smartcatcom/serge:v2 ^
     %*
